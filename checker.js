4864641aa33d2c5b2259c6bb63fa78bc6f302f67
Martin Desrumaux <martin.desrumaux@epitech.eu>
2015-11-20 23:25:22 +0100

    Add parameters fix #1

--- checker.js
+++ checker.js
@@ -9,7 +9,7 @@ function isLinuxSupported(appid) {
     if (!error && response.statusCode == 200) {
       var completeData = JSON.parse(body);
       var data = completeData[Object.keys(completeData)[0]].data;
-      console.log("Linux supported ? " + data.platforms.linux);
+      console.log("Linux supported for " + data.name + " ? " + data.platforms.linux);
       if (data.platforms.linux === true) {
 	notify.notify("Supported !", {
 	  title: "Steam checker - " + data.name,
@@ -30,6 +30,40 @@ function isLinuxSupported(appid) {
   });
 }
 
+var interval = 30;
+var appid = 255220;
+(function() {
+  var index = 2; // let's supposed you launched it with node.js checker.js -i 30 255220
+  while (index < process.argv.length) {
+    if (process.argv[index] == "--help") {
+      console.log("Usage: node checker.js -i [interval of checking in seconds] [appid]");
+      console.log("When there is no parameters, the program will check for Grid Autosport every 30 seconds");
+      console.log("Example 1 : node checker.js -i 30 255220");
+      console.log("Example 2 : node checker.js -i 5 570");
+      process.exit();
+    } else if (process.argv[index] == "-i") {
+      interval = parseInt(process.argv[index + 1]);
+      index++;
+    } else {
+      appid = parseInt(process.argv[index]);
+    }
+    index++;
+  }
+  if (isNaN(interval)) {
+    console.error("Interval have to be an integer.");
+    process.exit();
+  }
+
+  if (isNaN(appid)) {
+    console.error("Appid have to be a number.");
+    process.exit();
+}
+/*
+console.log("appid : " + appid)
+console.log("interval : " + interval)
+*/
+})();
+
 setInterval(function() {
-  isLinuxSupported(255220);
-}, 1000 * 5);
+  isLinuxSupported(appid);
+}, 1000 * interval);
